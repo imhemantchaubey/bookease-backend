03683158dcd7d80d3b1cb7d8d69866707bcfd247
imhemantchaubey <codewithhemant@gmail.com>
2023-05-18 20:20:15 +0530

    feat: add connection to mongodb database using node.js and express

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import chalk from "chalk"
 
 const app = express()
+dotenv.config()
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.URI)
+  } catch (error) {
+    throw error
+  }
+}
+
+mongoose.connection.on("disconnected", () => {
+  console.log(chalk.bgRed("Connection to the database has been lost..."))
+})
+
+mongoose.connection.on("connected", () => {
+  console.log(chalk.bgGreen("Successfully connected to database!"))
+})
 
 app.listen(8081, () => {
-  console.log("Server is running on port 8081!")
+  connect()
+  console.log(chalk.bgYellow("Server is running on port 8081!"))
 })
